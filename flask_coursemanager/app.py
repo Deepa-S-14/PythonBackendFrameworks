15af6a60e4fa19c121a69938170082a226a5503b
Deepa <deepaselvam2023@gmail.com>
2026-06-28 19:43:43 +0530

    Handson 4 - Flask REST API

---
+++ flask_coursemanager/app.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from config import Config
+from courses.routes import courses_bp
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    app.register_blueprint(courses_bp)
+
+    @app.errorhandler(400)
+    def bad_request(error):
+        return {"error": "Bad Request"}, 400
+
+    @app.errorhandler(404)
+    def not_found(error):
+        return {"error": "Resource not found"}, 404
+
+    return app
+
+
+app = create_app()
+
+if __name__ == "__main__":
+    app.run(debug=True)
