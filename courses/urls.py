bf9025eb2d761ccd584bda375ed99c91c1a053a2
Deepa <deepaselvam2023@gmail.com>
2026-06-28 16:36:23 +0530

    Handson 3 - Django REST Framework APIs

--- courses/urls.py
+++ courses/urls.py
@@ -1,6 +1,18 @@
-from django.urls import path
-from .views import hello_view
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import (
+    DepartmentViewSet,
+    CourseViewSet,
+    StudentViewSet,
+    EnrollmentViewSet,
+)
+
+router = DefaultRouter()
+router.register(r'departments', DepartmentViewSet)
+router.register(r'courses', CourseViewSet)
+router.register(r'students', StudentViewSet)
+router.register(r'enrollments', EnrollmentViewSet)
 
 urlpatterns = [
-    path("hello/", hello_view),
+    path('', include(router.urls)),
 ]
--- courses/views.py
+++ courses/views.py
@@ -1,4 +1,32 @@
+from rest_framework import viewsets
+from .models import Department, Course, Student, Enrollment
+from .serializers import (
+    DepartmentSerializer,
+    CourseSerializer,
+    StudentSerializer,
+    EnrollmentSerializer
+)
+
 from django.http import HttpResponse
 
 def hello_view(request):
-    return HttpResponse("Course Management API is running")
+    return HttpResponse("Course Management API is running") 
+
+class DepartmentViewSet(viewsets.ModelViewSet):
+    queryset = Department.objects.all()
+    serializer_class = DepartmentSerializer
+
+
+class CourseViewSet(viewsets.ModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+
+class StudentViewSet(viewsets.ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+
+class EnrollmentViewSet(viewsets.ModelViewSet):
+    queryset = Enrollment.objects.all()
+    serializer_class = EnrollmentSerializer
